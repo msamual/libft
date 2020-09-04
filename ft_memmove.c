e24634571ead026c731eb50026d6aacfe0bf1ff0
msamual74 <suryashev@bk.ru>
2020-09-04 10:48:40 +0500

    move

---
+++ ft_memmove.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+void	*ft_memmove(void *dest, const void *source, size_t size)
+{
+	unsigned char	*tmp;
+	unsigned char	*destination;
+	unsigned char	*src;
+	size_t		i;
+
+	tmp = malloc(sizeof(unsigned char) * size);
+	if (!tmp)
+		return (dest);
+	destination = (unsigned char *)dest;
+	src = (unsigned char *)source;
+	i = -1;
+	while (++i < size)
+		tmp[i] = src[i];
+	i = -1;
+	while (++i < size)
+		destination[i] = tmp[i];
+	return (dest);
+}

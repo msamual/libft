c14cc4b22dc0c532bc51e06a49162cbbc7644650
msamual74 <suryashev@bk.ru>
2020-09-06 11:42:39 +0500

    lendup

---
+++ ft_strdup.c
@@ -0,0 +1,15 @@
+
+#include "libft.h"
+
+char	*ft_strdup(const char *str)
+{
+	char	*res;
+	int	i;
+
+	i = -1;
+	if (!(res = malloc(sizeof(char) * ft_strlen(str))))
+		return (NULL);
+	while (str[++i])
+		res[i] = str[i];
+	return (res);
+}

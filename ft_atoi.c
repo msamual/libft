dd2ab856d738305e6466bd1468d066cd18ef7d08
msamual74 <suryashev@bk.ru>
2020-09-10 10:46:42 +0500

    atoi

---
+++ ft_atoi.c
@@ -0,0 +1,23 @@
+
+int	ft_atoi(char *str)
+{
+	int neg;
+	int res;
+
+	neg = 0;
+	res = 0;
+	while (*str == ' ' || (*str > 8 && *str < 14))
+			str++;
+	if (*str == '-')
+		neg++;
+	if (*str == '-' || *str == '+')
+		str++;
+	while (*str > 47 && *str < 58)
+	{
+		res = res * 10 + (*str - 48);
+		str++;
+	}
+	if (neg)
+		return (-res);
+	return (res);
+}

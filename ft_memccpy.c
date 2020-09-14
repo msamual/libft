7e76d862b3bb1a430d978a6b06332d831b3e22dd
msamual74 <suryashev@bk.ru>
2020-09-14 09:35:01 +0500

    BEBE

--- ft_memccpy.c
+++ ft_memccpy.c
@@ -3,17 +3,18 @@
 
 void	*ft_memccpy(void *dest, const void *src, int c, size_t size)
 {
-	size_t		i;
-	unsigned char	*source;
-	unsigned char	*destiation;
+	unsigned char		*source;
+	unsigned char		*destination;
 
 	source = (unsigned char *)src;
 	destination = (unsigned char *)dest;
-	if (size < 1 || src == dest)
-		return (dest);
-	i = -1;
-	while (++i < size && source[i] != (unsigned char)c)
-		destination[i] = source[i];
-	destination[i] = source[i];
-	return (dest);
+	while (size--)
+	{
+		*destination = *source;
+		if (*source == (unsigned char)c)
+			return (destination + 1);
+		destination++;
+		source++;
+	}
+	return (NULL);
 }

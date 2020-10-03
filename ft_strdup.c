616bb2367ea011337ed48e0c324c7658c4790072
msamual74 <suryashev@bk.ru>
2020-10-04 01:14:52 +0500

    strdup

--- ft_strdup.c
+++ ft_strdup.c
@@ -7,9 +7,10 @@ char		*ft_strdup(const char *str)
 	int		i;
 
 	i = -1;
-	if (!(res = malloc(sizeof(char) * ft_strlen(str))))
+	if (!(res = malloc(sizeof(char) * ft_strlen(str) + 1)))
 		return (NULL);
 	while (str[++i])
 		res[i] = str[i];
+	res[i] = '\0';
 	return (res);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -8,11 +8,13 @@ char	*ft_strstr(const char *haystack, const char *needle)
 	int k;
 
 	i = 0;
+	if (*needle == '\0')
+		return ((char *)haystack);
 	while (haystack[i])
 	{
 		j = i;
 		k = 0;
-		while (haystack[j] == needle[k])
+		while (haystack[j] && haystack[j] == needle[k])
 		{
 			j++;
 			k++;

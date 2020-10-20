d29fc0ef1423776e61e7f7ea541e0cd34dfde398
msamual74 <suryashev@bk.ru>
2020-10-20 14:52:03 +0500

    lstdel

---
+++ ft_lstdel.c
@@ -0,0 +1,11 @@
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	if (!alst || !del)
+		return ;
+	if ((*alst)->next)
+		ft_lstdel((&(*alst)->next), del);
+	ft_lstdelone(alst, del);
+}
